a0d25c0db85a006f7f603e090fd6775abc9d2f63
xiaohuoni <448627663@qq.com>
2019-04-01 16:34:56 +0800

    fix config

--- plugin.js
+++ plugin.js
@@ -1,5 +1,6 @@
-
 module.exports = (api, options = {}) => {
+  console.log(api.config);
+
   api.registerPlugin({
     id: 'sekiro:antd',
     apply: require('umi-plugin-react/lib/plugins/antd').default,
@@ -9,6 +10,18 @@ module.exports = (api, options = {}) => {
     id: 'sekiro:dva',
     apply: require('umi-plugin-react/lib/plugins/dva').default,
     opts: options.dva,
+  });
+
+  api._registerConfig(() => {
+    return () => {
+      return {
+        name: 'antd',
+        validate: () => true,
+        onChange(newConfig) {
+          api.service.restart(`antd changed`);
+        },
+      };
+    };
   });
 
   api.onBuildSuccess(() => {
